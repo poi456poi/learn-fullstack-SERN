3e05bfbddfff5574ce32a23faaa25eece0334cab
unknown <sonphanthai2002@gmail.com>
2023-06-22 07:41:54 +0700

    install sequelize-cli and install db

--- src/route/web.js
+++ src/route/web.js
@@ -5,7 +5,7 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
-    router.get('/about', homeController.getAboutPage);
+
 
     return app.use("/", router);
 }
